ce01f0d92b74046450944026ca43b479573a67bb
Nikita Lukjanov <nikita.lukjanov@nandos.co.uk>
2022-03-20 18:22:19 +0000

    feat: add server search filtering

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static('dist'));
 
-app.get('/api/items', (req, res) => res.send({ items }));
+app.get('/api/items', (req, res) => {
+  const searchedItems = items.filter((filteredItem) => {
+    return filteredItem.name.toLowerCase().includes(req.query.search);
+  });
+  if (req.query.search === '') {
+    return res.send({ items });
+  }
+  return res.send({ items: searchedItems });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
